a94d392459cb9cc46418e34cbb2230e0bede0681
alemesh <alemesh@acceptic.com>
2018-08-02 18:06:57 +0300

    finish desctop

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,21 @@
 $(function() {
 
-	// Custom JS
+    //meny mobile
+    $(".toggle-mnu").click(function() {
+        $(this).toggleClass("on");
+        $(".main-mnu").slideToggle();
+        // return false;
+    });
+    //mobile menu scroll
+    window.onscroll = function() {
+        var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if(scrolled > 0){
+            $('.toggle-mnu').addClass('background-button');
+        }
+        if(scrolled <= 0){
+            $('.toggle-mnu').removeClass('background-button');
+        }
+    };
+
 
 });
